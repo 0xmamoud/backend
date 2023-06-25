dba57f587bb20eb7188f84f329804a729ba58832
KANE <mahamadoudaba.kane@gmail.com>
2023-06-26 00:58:45 +0200

    mise en place de l'authentification

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Thing = require("./models/Thing");
+const stuffRoutes = require("./routes/stuff");
+const usersRoutes = require("./routes/user");
 require("dotenv").config();
 
 const USER = process.env.NODE_APP_USER;
@@ -33,52 +34,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// post item
-app.post("/api/stuff", (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// modifie item
-app.put("/api/stuff/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// delete one item
-app.delete("/api/stuff/:id", (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then((thing) =>
-      res.status(200).json({ messsage: "supression effectué !" })
-    )
-    .catch((error) =>
-      res.status(400).json({ messsage: "supression effectué !" })
-    );
-});
-
-// get  one item
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => {
-      res.status(200).json(thing);
-    })
-    .catch((error) => {
-      res.status(404).json({ error });
-    });
-});
-
-// get every item
-app.get("/api/stuff", (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", usersRoutes);
 
 module.exports = app;
